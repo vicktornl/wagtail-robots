9b30947d348bcf83c3c77535f4c6b88df0ab3a33
Storm B. Heg <storm@stormbase.digital>
2025-09-29 15:19:39 +0200

    Use BigAutoField

---
+++ robots/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class RobotsConfig(AppConfig):
+    name = 'robots'
+    verbose_name = "Robots.txt Management"
+    default_auto_field = 'django.db.models.BigAutoField'
